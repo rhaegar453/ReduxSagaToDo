b04b20c35329cea830da2845280a86f9fa0575ea
Shivaraj Bakale <shivarajapple@gmail.com>
2019-09-21 17:26:51 +0530

    Notification system ready

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {failureAlert, failureNotification, modalToggle,successAlert,successNotification,warningNotification} from '../Store/UIStore/Actions';
 
-
-export default class Content extends React.Component {
+class Content extends React.Component {
     constructor(props) {
         super(props);
     }
@@ -10,13 +11,25 @@ export default class Content extends React.Component {
         return (
             <div>
                 <h4><u>Notifications</u></h4>
-                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}}>Warning Notification</button>
-                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}}>Success Notification</button>
-                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}}>Failure Notification</button>
+                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}} onClick={()=>this.props.warningNotification({message:"Some random"})} >Warning Notification</button>
+                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}} onClick={()=>this.props.successNotification({message:"Some random"})} >Success Notification</button>
+                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}} onClick={()=>this.props.failureNotification({message:"Some random"})} >Failure Notification</button>
                 <h4><u>Alerts</u></h4>
-                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}}>Success Alert</button>
-                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}}>Failure Alert</button>
+                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}} onClick={()=>this.props.successAlert({message:"Some random message"})} >Success Alert</button>
+                <button className="btn btn-primary" style={{marginLeft:'5px', marginTop:'10px'}} onClick={()=>this.props.failureAlert({message:"Some random message"})} >Failure Alert</button>
             </div>
         );
     }
 }
+
+const mapDispatchToProps=(dispatch)=>{
+    return {
+        successNotification:({message})=>dispatch(successNotification({message})),
+        failureNotification:({message})=>dispatch(failureNotification({message})),
+        warningNotification:({message})=>dispatch(warningNotification({message})),
+        successAlert:({message})=>dispatch(successAlert({message})),
+        failureAlert:({message})=>dispatch(failureAlert({message}))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Content);
--- src/Store/UIStore/Reducers.js
+++ src/Store/UIStore/Reducers.js
@@ -2,31 +2,32 @@ import * as actions from './ActionTypes';
 import Swal from 'sweetalert2';
 import toastr from 'toastr';
 
-const initialStore={
-    globalLoading:false,
-    modalOpen:false
+
+const initialStore = {
+    globalLoading: false,
+    modalOpen: false
 }
 
 
-const reducer=(state=initialStore, action)=>{
-    switch(action.type){
+const reducer = (state = initialStore, action) => {
+    switch (action.type) {
         case actions.successAlert:
-            Swal.fire({type:'success', timer:'1500', title:action.payload})
+            Swal.fire({ type: 'success', timer: '1500', title: action.payload })
             return state;
         case actions.failureAlert:
-            Swal.fire({type:'error', timer:'1500', title:action.payload});
+            Swal.fire({ type: 'error', timer: '1500', title: action.payload });
             return state;
         case actions.successNotification:
-            toastr.success(action.payload, "Success");
+            toastr.success(action.payload, "Success", {timeOut:'2000', progressBar:true,newestOnTop:true, closeButton:true});
             return state;
         case actions.failureNotification:
-            toastr.error(action.payload, "Failure");
+            toastr.error(action.payload, "Failure", {timeOut:'2000', progressBar:true,newestOnTop:true, closeButton:true});
             return state;
         case actions.warningNotification:
-            toastr.warning(action.payload, "Warning");
+            toastr.warning(action.payload, "Warning", {timeOut:'2000', progressBar:true,newestOnTop:true, closeButton:true});
             return state;
         case actions.modalToggle:
-            return {...state, modalOpen:!state.modalOpen}
+            return { ...state, modalOpen: !state.modalOpen }
         default:
             return state;
     }
